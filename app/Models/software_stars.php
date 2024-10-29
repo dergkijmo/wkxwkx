70a6f1b5d48bba7e1cac8f9f410f416f59c3c77d
petrichar777 <745120634@qq.com>
2024-10-29 08:33:14 +0800

    xxx

--- app/Models/software_stars.php
+++ app/Models/software_stars.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class students extends Model
+class software_stars extends Model
 {
     use HasFactory;
 }
